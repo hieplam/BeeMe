9b77bb49ced030bba595cfc9fbab4af6270e5d2f
Hip <lamhiep16@gmail.com>
2016-04-09 00:23:09 +0700

    test 1

--- SeeMe/SeeMe/Migrations/Configuration.cs
+++ SeeMe/SeeMe/Migrations/Configuration.cs
@@ -13,6 +13,19 @@ namespace SeeMe.Migrations
 
         protected override void Seed(DataContext context)
         {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+
             if (!context.Artists.Any())
             {
                 context.Artists.AddOrUpdate(a => a.Id, new Artist
