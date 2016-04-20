ace679bb6bf5f91aae727c6fe24a47596166287d
Hip <lamhiep16@gmail.com>
2016-04-20 21:45:27 +0700

    Fix EF6 reference issue

--- SeeMe/SeeMe.Repositories/DataContext.cs
+++ SeeMe/SeeMe.Repositories/DataContext.cs
@@ -11,6 +11,9 @@ namespace SeeMe.Repositories
 
         public DataContext() : base("SeeMe")
         {
+            //Refer to http://stackoverflow.com/questions/14033193/entity-framework-provider-type-could-not-be-loaded/19130718#19130718 
+            //If no below code, will make error.
+            var instance = System.Data.Entity.SqlServer.SqlProviderServices.Instance;
             Configuration.LazyLoadingEnabled = false;
             //Configuration.AutoDetectChangesEnabled = false;
             
